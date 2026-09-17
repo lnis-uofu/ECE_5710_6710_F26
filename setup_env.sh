16353ac5ce8539484a0424aa9737e56898a1aa1d
jay <jaygraham.mt@gmail.com>
2026-09-17 13:11:06 -0600

    Update lab setup: fix setup script typo and add secure NEX wrapper

--- setup_env.sh
+++ setup_env.sh
@@ -8,12 +8,10 @@ endif
 
 if ( ! (-f ${cwd}/.git || -f ${cwd}/setup_env.sh) ) then
   echo "ERROR: You must go to the root directory of your GIT project database"
-  echo "       where the setup_env.csh file is located."
+  echo "       where the setup_env.sh file is located."
   exit 1
 endif
 ##############################################################################
-
-
 
 setenv pdk_dir /research/ece/lnis-teaching/Designkits/tsmc180nm/pdk
 setenv cadence_base /uusoc/facility/cad_tools/Cadence/IC6-15
@@ -30,7 +28,6 @@ setenv CDS_Netlisting_Mode Analog
        setenv LD_LIBRARY_PATH $CDS/tools/lib/32bit
    endif
 
-
 source /uusoc/facility/cad_tools/Mentor/mentor_setup.sh
 source /uusoc/facility/cad_tools/Cadence/cadence_setup.sh
 
@@ -43,4 +40,3 @@ echo "Genus ......... : 21.15-s080_1"
 echo "Virtuoso ...... : 6.1.8-64b"
 echo "Innovus ....... : v21.15-s110_1"
 echo "----------------------------------------------------------------"
-
